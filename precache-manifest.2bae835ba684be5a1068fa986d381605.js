28d9598d70d7c07425cd494f62893280723309eb
hxm108 <hxm2899822@foxmail.com>
2020-03-01 00:37:04 +0800

    Kernel upgrade

---
+++ precache-manifest.2bae835ba684be5a1068fa986d381605.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ba6bd448f295208b28b8f1b9ee4772",
+    "url": "a6d6ce45ea428a3801d5.worker.js"
+  },
+  {
+    "revision": "83cfd500caa3d48ad790",
+    "url": "css/app.704cf520.css"
+  },
+  {
+    "revision": "4ae6c5b6f3302ed45caf",
+    "url": "css/chunk-vendors.a870fc53.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "540a757fdf80db4f4cc0e42341a1b402",
+    "url": "index.html"
+  },
+  {
+    "revision": "83cfd500caa3d48ad790",
+    "url": "js/app.7f40e776.js"
+  },
+  {
+    "revision": "4ae6c5b6f3302ed45caf",
+    "url": "js/chunk-vendors.27517252.js"
+  },
+  {
+    "revision": "4bb9486431782a6e8db809427724d18c",
+    "url": "manifest.json"
+  }
+]);
